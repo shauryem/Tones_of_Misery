4fef451b3addefd607c450ae865e06ff44e256f4
Shaurye Mahajan <43662406+shauryem@users.noreply.github.com>
2020-08-13 11:16:07 -0700

    Update Comments

--- DeathMenu.cs
+++ DeathMenu.cs
@@ -4,13 +4,14 @@ using System.Collections;
 public class DeathMenu : MonoBehaviour {
 
 	public string mainMenuLevel;
-
+	
+	// Restarts game and score
 	public void RestartGame()
 	{
 		FindObjectOfType<GameManager> ().Reset ();
 
 	}
-
+	// Quits to main menu
 	public void QuitToMain ()
 	{
 		Application.LoadLevel (mainMenuLevel);
