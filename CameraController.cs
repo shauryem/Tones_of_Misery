9c29d6e9c7ed634fb714e1ec6e9889965f900d74
Shaurye Mahajan <43662406+shauryem@users.noreply.github.com>
2020-08-13 11:14:23 -0700

    Update CameraController.cs

--- CameraController.cs
+++ CameraController.cs
@@ -4,12 +4,11 @@ using System.Collections;
 public class CameraController : MonoBehaviour {
 
 	public PlayerController thePlayer;
-
 	private Vector3 lastPlayerPosition;
 	private float distanceToMove;
 
 
-	// Use this for initialization
+	// called at the start of each game
 	void Start () {
 		thePlayer = FindObjectOfType <PlayerController> ();
 		lastPlayerPosition = thePlayer.transform.position;
@@ -18,11 +17,10 @@ public class CameraController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+		// updates the camera to move with the player
+		
 		distanceToMove = thePlayer.transform.position.x - lastPlayerPosition.x;
-
-
 		transform.position = new Vector3 (transform.position.x + distanceToMove, transform.position.y, transform.position.z);
-			
 		lastPlayerPosition = thePlayer.transform.position;
 	}
 }
