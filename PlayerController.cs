dfdd61e97bf624bd4bb95cdef080cb1df20c0f36
Shaurye Mahajan <43662406+shauryem@users.noreply.github.com>
2020-08-13 11:34:39 -0700

    Update Comments

--- PlayerController.cs
+++ PlayerController.cs
@@ -7,85 +7,73 @@ public class PlayerController : MonoBehaviour {
 	private float moveSpeedStore;
 	private float speedMileStoneCountStore;
 	public float speedMultiplier;
-
 	public float speedIncreaseMilestone;
 	private float speedMilestoneCount;
 	public float speedIncreaseMilestoneStore;
-
 	public float jumpForce;
-
-
 	private bool stoppedJumping;
-
 	public float jumpTime;
 	private float jumpTimeCounter;
-
 	private Rigidbody2D myRigidbody;
-
 	public AudioSource jumpSound;
 	public AudioSource deathSound;
-
-
 	public bool grounded;
-
 	public LayerMask whatIsGround;
 	public Transform groundCheck;
 	public float groundCheckRadius;
-
-	//private Collider2D myCollider;
-
 	private Animator myAnimator;
-
 	public GameManager theGameManager;
 
-	// Use this for initialization
+	// Called at the start of each game
+	
 	void Start () {
+		// Initializing player variables
+		
 		myRigidbody = GetComponent<Rigidbody2D>();
-
-		//myCollider = GetComponent<Collider2D> ();
-
 		myAnimator = GetComponent<Animator> ();
-	
 		jumpTimeCounter = jumpTime;
-
 		speedMilestoneCount = speedIncreaseMilestone;
-	
 		moveSpeedStore = moveSpeed;
 		speedMileStoneCountStore = speedMilestoneCount;
 		speedIncreaseMilestoneStore = speedIncreaseMilestone;
-
 		stoppedJumping = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		//grounded = Physics2D.IsTouchingLayers (myCollider, whatIsGround);
+		// Checks if the player is on the ground
+		
 		grounded = Physics2D.OverlapCircle (groundCheck.position, groundCheckRadius, whatIsGround);
-
+		
+		// Increases player speed based on distance traveled
+		
 		if (transform.position.x > speedMilestoneCount) {
 		
 			speedMilestoneCount += speedIncreaseMilestone;
-
 			speedIncreaseMilestone = speedIncreaseMilestone * speedMultiplier;
 			moveSpeed = moveSpeed * speedMultiplier;
 		}
 
 		myRigidbody.velocity = new Vector2 (moveSpeed, myRigidbody.velocity.y);
-
+		
+		// Jump if space bar is pressed OR if clicked on mobile
+		
 		if(Input.GetKeyDown (KeyCode.Space)|| Input.GetMouseButtonDown(0))
 		{
-
+			// only can jump if player is on the ground
+			
 			if (grounded) 
 			{
-
-
 				myRigidbody.velocity = new Vector2 (myRigidbody.velocity.x, jumpForce);
 				stoppedJumping = false;
 				jumpSound.Play();
-			
+
 			}
-			}
+		}
+		
+		// Jumps higher depending on how long button is pressed
+		
 		if ((Input.GetKey (KeyCode.Space) || Input.GetMouseButton (0)) && !stoppedJumping){
 		
 			if (jumpTimeCounter > 0) {
@@ -109,7 +97,9 @@ public class PlayerController : MonoBehaviour {
 		myAnimator.SetFloat ("Speed", myRigidbody.velocity.x);
 		myAnimator.SetBool ("Grounded", grounded);
 	}
-
+	
+	// Kills player and restarts game if they hit spike
+	
 	void OnCollisionEnter2D (Collision2D other)
 	{
 		if (other.gameObject.tag == "killbox") {
